2f9a6817964c4c47853b32162af6a13f607d2b21
Luciano Filho <lvgalvaofilho@gmail.com>
2023-02-05 17:44:36 -0300

    add player module

---
+++ collections/player.py
@@ -0,0 +1,9 @@
+from collections import namedtuple
+
+# j = namedtuple('jogador', 'nome time camisa n')
+
+# n = namedtuple('ABC', 'slot1 slot2 slo3')
+
+naipes = 'P C O E'.split()
+valores = list(range(2,11)) + 'A J Q K'.split()
+
